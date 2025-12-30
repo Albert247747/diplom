694293fed5327e6bb97f9e84d7bd499634cf7659
gatia <gatiatullinalbert9@gmail.com>
2025-12-30 16:31:52 +0300

    crate admin panel

---
+++ lib/ui/common/theme/styleText.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color blackText =Colors.black;
+
+const textTheme =TextTheme(
+titleMedium: TextStyle(fontSize: 24, color: blackText)
+);
+
+extension TextStyles on BuildContext {
+
+  TextStyle get titleMedium => Theme.of(this).textTheme.titleMedium!;
+
+}
